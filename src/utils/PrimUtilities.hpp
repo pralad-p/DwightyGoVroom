e1fb57a625e8d36a30c8bbed710c180458e84640
Pralad Prasad <pralad.prasad@gmail.com>
2023-09-05 21:24:49 +0200

    add logger macro

--- src/utils/PrimUtilities.hpp
+++ src/utils/PrimUtilities.hpp
@@ -5,9 +5,20 @@
 #ifndef DWIGHTYGOVROOM_PRIMUTILITIES_HPP
 #define DWIGHTYGOVROOM_PRIMUTILITIES_HPP
 
+// Includes
+#include "Goal.hpp"
+#include <spdlog/spdlog.h>
+
+// User definitions
 #define DGV_GOALS_PATH (std::filesystem::current_path() / "GOALS_DB.json")
 
-#include "Goal.hpp"
+#define LOG_CRITICAL(message) do { \
+    auto logger = spdlog::get("default_file_logger"); \
+    if (logger) { \
+        logger->critical(message); \
+    } \
+} while(0)
+
 
 std::string QuadrantStateEnum2String(QuadrantStateEnum);
 
