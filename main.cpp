f9617fa3d913f7cb423439406abe0917e4976540
Pralad Prasad <pralad.prasad@gmail.com>
2023-09-09 20:18:39 +0200

    complete main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
+#include <spdlog/spdlog.h>
+#include "spdlog/sinks/basic_file_sink.h"
+#include "ControllerEngine.hpp"
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    // Set logger
+    auto logger = spdlog::basic_logger_mt("default_file_logger", "logs/tui-log.txt", true);
+
+    // Start event loop
+    ControllerEngine::getInstance().start();
+
+    // Stop event loop
+    ControllerEngine::getInstance().stop();
+
+    // Shutdown logger
+    spdlog::shutdown();
     return 0;
 }
